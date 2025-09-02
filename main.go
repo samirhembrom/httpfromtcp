cefa17308d778fe7e34e5b39ed68bfe9283ff121
samirhembrom <samirhembrom007@gmail.com>
2025-09-02 21:49:36 +0530

    ch1:l5

--- main.go
+++ main.go
@@ -1,35 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	f, err := os.Open("messages.txt")
-	if err != nil {
-		fmt.Printf("%s", err)
-		return
-	}
-	defer f.Close()
+const inputFilePath = "messages.txt"
 
-	var line string
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	line := make(chan string)
 
-	for {
-		data := make([]byte, 8)
-		_, err := f.Read(data)
-		if err != nil {
-			return
-		}
-
-		for _, s := range string(data) {
-			if s == '\n' {
-				fmt.Printf("read: %s\n", line)
-				line = ""
-				continue
+	go func() {
+		defer f.Close()
+		defer close(line)
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					line <- currentLineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
 			}
-			line += string(s)
-		}
 
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := range len(parts) - 1 {
+				line <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return line
+}
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s", inputFilePath, err)
+	}
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Printf("-------------------------------------\n")
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Println("read:", line)
 	}
 }
